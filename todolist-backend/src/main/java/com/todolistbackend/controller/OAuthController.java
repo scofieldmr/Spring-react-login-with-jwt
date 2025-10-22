625b98b52749ce8e7514e906fd93b3a111ea5387
Mohamed Bilal <bilalumar95@gmail.com>
2025-10-22 11:55:48 +0530

    Modified OAuth Controller

--- todolist-backend/src/main/java/com/todolistbackend/controller/OAuthController.java
+++ todolist-backend/src/main/java/com/todolistbackend/controller/OAuthController.java
@@ -1,4 +1,25 @@
 package com.todolistbackend.controller;
 
+import com.todolistbackend.OAuth.dto.OAuthUserDto;
+import com.todolistbackend.OAuth.service.OAuthServiceImp;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/v1/oauth")
+@CrossOrigin("*")
 public class OAuthController {
+
+    private final OAuthServiceImp oAuthServiceImp;
+
+    public OAuthController(OAuthServiceImp oAuthServiceImp) {
+        this.oAuthServiceImp = oAuthServiceImp;
+    }
+
+
+    // Google: frontend sends idToken
+
 }

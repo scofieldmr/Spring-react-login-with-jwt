ef9d327729538935744e04b9de374a568660b5e5
Mohamed Bilal <bilalumar95@gmail.com>
2025-10-22 09:16:37 +0530

    Made changes in app.jsx

--- todolist-app/src/App.jsx
+++ todolist-app/src/App.jsx
@@ -22,30 +22,31 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route element={<DashboardLayout />}>
+            {/* Public pages */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path='/email-verify' element={<EmailVerify />} />
+            <Route path='/otp-verify' element={<OTPVerify />} />
+            <Route path='/change-password' element={<ChangePassword />} />
+            <Route path='/reset-password' element={<ResetPassword />} />
 
-          {/* Public routes */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path='/email-verify' element={<EmailVerify />} />
-          <Route path='/otp-verify' element={<OTPVerify />} />
-          <Route path='/change-password' element={<ChangePassword />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-
-          {/* Protected layout */}
-          <Route element={<ProtectedRoute />}>
-            <Route element={<DashboardLayout />}>
+            {/* Protected routes */}
+            <Route element={<ProtectedRoute />}>
               <Route path="/user-dashboard" element={<UserDashboard />} />
               <Route path="/mod-dashboard" element={<ModDashboard />} />
               <Route path="/admin-dashboard" element={<AdminDashboard />} />
               <Route path="/profile-details" element={<ProfileDetails />} />
             </Route>
-          </Route>
+            </Route>
 
-          {/* Redirect root to login */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
+                      {/* Default route → Login */}
+             <Route path="/" element={<Navigate to="/login" replace />} />
 
-          <Route path="/unauthorized" element={<h2>Unauthorized Access 🚫</h2>} />
+            {/* Unauthorized */}
+            <Route path="/unauthorized" element={<h2>Unauthorized Access 🚫</h2>} />
 
+           
         </Routes>
       </BrowserRouter>
     </>
